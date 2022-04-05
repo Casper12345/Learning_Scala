1723cbfd2c76632009fb1a403335148924568141
aparsonstrew <apa@dixa.com>
2022-04-05 14:55:50 +0200

    exercise answers

--- src/main/scala/collections/Exercises.scala
+++ src/main/scala/collections/Exercises.scala
@@ -1,6 +1,6 @@
 package collections
 
-object Exercises {
+object Exercises extends App{
 
   // 1.  given a list of records sort the records ascending and return the sum of the values
   case class Record(id: Char, value: Int)
@@ -13,7 +13,8 @@ object Exercises {
     Record('a', 9),
   )
 
-  val ans1 = input.sortWith()
+  val ans1 = input.sortWith( _.id < _.id ).foldLeft(0)((i, r) => i + r.value)
+  //println(ans1)
 
 
   // 2. Given a list of records group them by id and multiply the grouped values by a factor 2
@@ -28,6 +29,9 @@ object Exercises {
     Record('d', 3),
     Record('d', 1),
   )
+
+  val ans2 = input2.groupBy(r => r.id).map { case (c, xs) => c -> xs.map(r => Record(r.id, r.value * 2)) }
+  //println(ans2)
 
   // 3. Given a list of records collect the charaters into a string and sum the values
 
@@ -45,6 +49,10 @@ object Exercises {
     Record('d', 8),
   )
 
+  val ans3A = input3.foldLeft("")((a, r) => a + r.id)
+  val ans3B = input3.foldLeft(0)((a, r) => a + r.value)
+  //println(s"$ans3A, $ans3B")
+
   // 4. Given a list of records return all records with values higher than 9
 
   val input4 = List(
@@ -60,7 +68,12 @@ object Exercises {
     Record('c', 1)
   )
 
+  val ans4 = input4.filter(r => r.value > 9)
+  //println(ans4)
+
   // 5. Given the same list of records return all records with values higher than 8 and where charater is a
+   val ans5 = input4.filter((r => r.value > 8 && r.id == 'a'))
+  //println(ans5)
 
   // 6. Given two list of records return one list contain a tuple with a string and the sum of values eg output: List(("hello world", sum))
 
@@ -79,5 +92,8 @@ object Exercises {
     Record('l', 1),
     Record('d', 8),
   )
+
+  val ans6 = (input5 ++ input6).foldLeft(List(("", 0)))((a, r) => List((a.head._1 + r.id, a.head._2 + r.value)))
+  //println(ans6)
 
 }
